2c6eca60d2c6833c4f113682ac4fa71d18aacdf5
Manish Gopal Singh <manish.singh707@gmail.com>
2022-01-19 20:29:56 +0545

    add routing

---
+++ src/controllers/product/getProducts.ts
@@ -0,0 +1,16 @@
+import { NextFunction } from "connect";
+import { Response, Request } from "express";
+
+export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const data = [{
+            id: '',
+            name: '',
+            price:'',
+            stock: ''
+        }];
+        res.json({data, status:'OK'});
+    }catch(e:any){
+       next(e);
+    }
+};
---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import {getProducts} from "../controllers/product";
+import {purchaseProduct, refundProduct} from "../controllers/purchase";
+
+const app = express();
+
+app.get("/products", getProducts);
+
+app.post("/purchase", purchaseProduct);
+
+app.post("/refund", refundProduct);
+
+export default app;
