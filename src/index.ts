4fc9934a4269e1c42a5c7740f65cf88e9ca9dcd0
Manish Gopal Singh <manish.singh707@gmail.com>
2022-01-22 20:35:20 +0545

    integrate UI (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import 'dotenv/config';
-import express, { Application } from 'express';
+import express from 'express';
+import next from 'next';
 import { APP_PORT } from './config';
 import errorHandler from './exception/handler';
 import { HttpException } from './exception';
-import Router from './router';
+import APIRoutes from './router';
 import sequelize from './bootstrap/sequelize';
 
-const app: Application = express();
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
+const port = Number(APP_PORT || 3000);
+const server = express();
 
-const App = async () => {
+app.prepare().then(async () => {
   try {
-    app.use(express.json());
-    app.use('/api', Router);
-    app.use(errorHandler);
-    app.use('*', (req, res) => {
-      res.status(404).send('');
-    });
+    server.use(express.json());
+    server.use('/api', APIRoutes);
+    server.use(errorHandler);
+    server.all('*', (req, res) => handle(req, res));
     await sequelize.authenticate();
-    app.listen(APP_PORT, () => {
-      console.log(`Server is running on port ${APP_PORT}`);
+
+    server.listen(port, () => {
+      console.log('Server is running on port 3000');
     });
   } catch (e: any) {
     throw new HttpException(`Error while starting the server : ${e.message}`);
   }
-};
-
-App();
+});
