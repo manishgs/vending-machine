b58110493bff0bf5c9383f130b0bc30706440a52
Manish Gopal Singh <manish.singh707@gmail.com>
2022-01-22 21:51:52 +0545

    update pkg dep

--- src/bootstrap/sequelize.ts
+++ src/bootstrap/sequelize.ts
@@ -1,25 +1,9 @@
 import { Options, Sequelize } from 'sequelize';
-import {
-  APP_ENV, DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME,
-} from '../config';
 
-let options: Options;
-
-if (APP_ENV === 'test') {
-  options = {
-    dialect: 'sqlite',
-    storage: './db/db.sqlite3',
-  };
-} else {
-  options = {
-    host: DB_HOST,
-    database: DB_NAME,
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    dialect: 'postgres',
-    logging: false,
-  };
-}
+const options: Options = {
+  dialect: 'sqlite',
+  storage: './db/db.sqlite3',
+};
 
 const sequelize = new Sequelize(options);
 
